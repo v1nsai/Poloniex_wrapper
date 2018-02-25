305e8263a0286930c6553243c2013216e65bf9e1
Drew Riffle <andrewlriffle@protonmail.com>
2018-02-25 09:57:17 -0500

    Added options for status bar and HTML5 compatibility

--- app/src/main/java/com/example/doctor_ew/poloniex/MainActivity.java
+++ app/src/main/java/com/example/doctor_ew/poloniex/MainActivity.java
@@ -5,50 +5,45 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
    // @Override
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // remove status bar and make fullscreen
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Hide the giant pointless title bar
         getSupportActionBar().hide();
 
-        Window window = this.getWindow();
+        // finally change the status bar color if you want to keep it
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.holo_green_dark));
+        }
 
-// clear FLAG_TRANSLUCENT_STATUS flag:
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-
-// finally change the color
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.holo_green_dark));
-
-//        WebView webview = (WebView)findViewById(R.id.poloniex);
-//        webview.getSettings().setJavaScriptEnabled(true);
-//        webview.loadUrl("https://www.poloniex.com");
-//
-//        webview.setWebViewClient(new WebViewClient() {
-//            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-//                view.loadUrl(request.getUrl().toString());
-//                return false;
-//            }
-//        });
-
+        //Set up HTML5 compatible webview
         WebView myWebView = (WebView) findViewById(R.id.poloniex);
+        myWebView.setFocusable(true);
+        myWebView.setFocusableInTouchMode(true);
         myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        myWebView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        myWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setDatabaseEnabled(true);
+        myWebView.getSettings().setAppCacheEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         myWebView.setWebViewClient(new WebViewClient());
         myWebView.loadUrl("https://www.poloniex.com");
     }
